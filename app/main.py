c4cd475241b97cdf172ac4541495f4994af2a6a2
Raiawadhendra02 <raiawadhendra@gmail.com>
2026-02-09 02:48:41 +0530

    Added CORS middleware to fix frontend connection

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 
 from app.models import employee, attendance
-
 from app.routes import employee as employee_routes
 from app.routes import attendance as attendance_routes
 from app.routes import dashboard as dashboard_routes
 
 app = FastAPI(title="HRMS Lite API")
+
+# ---------------- CORS FIX ----------------
+origins = [
+    "http://localhost:5173",  # React local
+    "http://localhost:3000",
+    "https://your-frontend-url.onrender.com"  # later for deployment
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all for now
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# ------------------------------------------
 
 Base.metadata.create_all(bind=engine)
 
